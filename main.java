05f945bd49b9c1cc4f3502c02389a85d50895d29
yaps25505-cyber <yaps25505@gmail.com>
2026-05-04 17:07:41 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+/**
+ * Clase principal que ejecuta el programa.
+ */
+public class Main {
+
+    /**
+     * Método principal del programa.
+     * @param args argumentos de entrada
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+        // Bucle que muestra números del 1 al 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
